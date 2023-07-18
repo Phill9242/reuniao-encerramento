57530558cb89a4cf78cbe81fff4b8cb4efe48e22
Rodrigo Pretel <pretrod971@gmail.com>
2023-07-18 15:58:36 -0300

    adding dropdown

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const button = document.getElementById("render")
+const posts = document.getElementsByClassName("posts")
+const teste = document.getElementById("renderizar")
+console.log(teste)
+
+button.addEventListener("click", () =>{
+  posts.append(teste)
+})
