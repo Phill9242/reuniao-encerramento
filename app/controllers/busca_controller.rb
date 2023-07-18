eb6f156435c059e70a2c51af1bc81a13edb97342
phill9242 <philcarvalho@live.com>
2023-07-18 14:45:17 -0300

    feature: buscas com parâmetros

--- app/controllers/busca_controller.rb
+++ app/controllers/busca_controller.rb
@@ -3,7 +3,17 @@ class BuscaController < ApplicationController
   end
 
   def search
-    @posts = Post.where("dev LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    @posts = Post.all
+    @posts = @posts.where("dev LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
+    @posts = @posts.where(status: params[:status]) if params[:status] != 'todos'
+    @posts = @posts.where(good_bad: params[:good_bad]) if params[:good_bad] != 'todos'
+
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @posts = @posts.where(date: start_date..end_date)
+    end 
+
     render 'index'
   end
 end
